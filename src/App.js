a00cc75cec16b81ed158ca7efabd51e2f6e956b2
Richard Brooks <rickbrooks22@gmail.com>
2018-07-31 15:45:59 -0400

    created weekly and monthly components

--- src/App.js
+++ src/App.js
@@ -2,49 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import Task from './Task';
 import Data from './data.json'
+import WeeklyTask from './weeklyTask';
+import MonthlyTask from './monthlyTask';
 
 class App extends Component {
   render() {
-    let weeklyTasks = Data.HMS.Weekly;
-    let monthlyTasks = Data.HMS.Monthly;
+
+    
     let yearlyTasks = Data.HMS.Yearly;
     return ( 
       <div className = "App" >
         <h1>ALL IN ONE HMS APPLICATION</h1>
-        <div className = "Time">
-          <h2>Weekly</h2>
-          {
-            weeklyTasks.map(t =>{
-              return (
-                <Task 
-                  title={t.Title}
-                  dueDate={t.DueDate}
-                  description={t.Description}
-                />
-              )
-            })
-          }
-          <Task
-            title="test test test"
-            dueDate="12/18/2018"
-            description="Cut the front and back"
-           />
-        </div>
+
+        <WeeklyTask />
         <br/>
-        <div className = "Time">
-          <h2>Monthly</h2>
-          {
-            monthlyTasks.map(t => {
-              return(
-                <Task 
-                  title={t.Title}
-                  dueDate={t.DueDate}
-                  description={t.Description}
-                />
-              )
-            })
-          }
-        </div>
+        <MonthlyTask />
         <br/>
         <div className = "Time">
           <h2>Yearly</h2>
