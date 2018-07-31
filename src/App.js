6a0503f1fbf8fbbe2d7f5a7a420dce093bc409fc
Bhisma Bakhai <bhisma93@gmail.com>
2018-07-31 13:56:03 -0400

    added HMS components/data

--- src/App.js
+++ src/App.js
@@ -1,74 +1,64 @@
 import React, { Component } from 'react';
 import './App.css';
 import Task from './Task';
-
+import Data from './data.json'
 
 class App extends Component {
   render() {
+    let weeklyTasks = Data.HMS.Weekly;
+    let monthlyTasks = Data.HMS.Monthly;
+    let yearlyTasks = Data.HMS.Yearly;
     return ( 
       <div className = "App" >
         <h1>ALL IN ONE HMS APPLICATION</h1>
         <div className = "Time">
           <h2>Weekly</h2>
-          <Task />
-          <Task />
-          <Task />
-          <div className = "Tasks">
-            <h3>Title:</h3>
-              <p>Mow the lawn.</p>
-            <h3>Due Date:</h3>
-              <p>12/18/2018</p>
-            <h3>Description:</h3>
-              <p>Cut the front and back</p>
-          </div>
-          <div className = "Tasks">
-            <h3>Title:</h3>
-              <p>Take out the trash</p>
-            <h3>Due Date:</h3>
-              <p>12/18/2018</p>
-            <h3>Description</h3>
-              <p>Take trash to curb</p>
-          </div>
+          {
+            weeklyTasks.map(t =>{
+              return (
+                <Task 
+                  title={t.Title}
+                  dueDate={t.DueDate}
+                  description={t.Description}
+                />
+              )
+            })
+          }
+          <Task
+            title="test test test"
+            dueDate="12/18/2018"
+            description="Cut the front and back"
+           />
         </div>
         <br/>
         <div className = "Time">
           <h2>Monthly</h2>
-          <div className = "Tasks">
-            <h3>Title:</h3>
-              <p>Some Task</p>
-            <h3>Due Date:</h3>
-              <p>Some Date</p>
-            <h3>Description:</h3>
-              <p>Some Description</p>
-          </div>
-          <div className = "Tasks">
-            <h3>Title:</h3>
-              <p>Some Task</p>
-            <h3>Due Date:</h3>
-              <p>Some Date</p>
-            <h3>Description:</h3>
-              <p>Some Description</p>
-          </div>
+          {
+            monthlyTasks.map(t => {
+              return(
+                <Task 
+                  title={t.Title}
+                  dueDate={t.DueDate}
+                  description={t.Description}
+                />
+              )
+            })
+          }
         </div>
         <br/>
         <div className = "Time">
           <h2>Yearly</h2>
-          <div className = "Tasks">
-            <h3>Title:</h3>
-              <p>Some Task</p>
-            <h3>Due Date:</h3>
-              <p>Some Date</p>
-            <h3>Description:</h3>
-              <p>Some Description</p>
-          </div>
-          <div className = "Tasks">
-            <h3>Title:</h3>
-              <p>Some Task</p>
-            <h3>Due Date:</h3>
-              <p>Some Date</p>
-            <h3>Description:</h3>
-              <p>Some Description</p>
-          </div>
+          {
+            yearlyTasks.map(t => {
+              return(
+                <Task 
+                  title={t.Title}
+                  dueDate={t.DueDate}
+                  description={t.Description}
+                />
+              )
+            })
+          }
         </div>
       </div>
 
--- src/Task.js
+++ src/Task.js
@@ -4,11 +4,11 @@ const Task = (props) => {
     return (
         <div className = "Tasks">
             <h3>Title:</h3>
-              <p>Mow the lawn.</p>
+              <p>{props.title}</p>
             <h3>Due Date:</h3>
-              <p>12/18/2018</p>
+              <p>{props.dueDate}</p>
             <h3>Description:</h3>
-              <p>Cut the front and back</p>
+              <p>{props.description}</p>
         </div>
     )
 
