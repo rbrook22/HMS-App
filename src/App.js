02ec18597738f9bfe6697e2147059874b3045215
Richard Brooks <rickbrooks22@gmail.com>
2018-07-31 16:28:48 -0400

    added navbar component and button routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 import './App.css';
+import Nav from './nav';
 import WeeklyTask from './weeklyTask';
 import MonthlyTask from './monthlyTask';
 import YearlyTask from './yearlyTask';
+
 
 class App extends Component {
   render() {
     return ( 
       <div className = "App" >
         <h1>ALL IN ONE HMS APPLICATION</h1>
-        <WeeklyTask />
+        <Nav />
         <br/>
-        <MonthlyTask />
-        <br/>
-        <YearlyTask />
+
+        <Route path="/weekly" component={WeeklyTask} />
+        <Route path="/monthly" component={MonthlyTask} />
+        <Route path="/yearly" component={YearlyTask} />
       </div>
     );
   }
---
+++ src/nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Nav = (props) => {
+    return (
+        <div>
+            <Link to="/weekly"><input type="button" value="Weekly Items"/> </Link>
+            <Link to="/monthly"><input type="button" value="Monthly Items"/></Link>
+            <Link to="/yearly"><input type="button" value="Yearly Items"/></Link>
+        </div>
+    );
+}
+
+export default Nav;
