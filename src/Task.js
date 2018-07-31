63f700aadeb6e5f826bdfa1a7cd495cf8b548abb
Bhisma Bakhai <bhisma93@gmail.com>
2018-07-31 13:38:51 -0400

    test1

---
+++ src/Task.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Task = (props) => {
+    return (
+        <div className = "Tasks">
+            <h3>Title:</h3>
+              <p>Mow the lawn.</p>
+            <h3>Due Date:</h3>
+              <p>12/18/2018</p>
+            <h3>Description:</h3>
+              <p>Cut the front and back</p>
+        </div>
+    )
+
+}
+
+export default Task;
